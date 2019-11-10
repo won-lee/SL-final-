c7c2bd4ab2c07046e0837035224cb59f7accca08
Tsupoly <55301726+Tsupoly@users.noreply.github.com>
2019-11-09 19:37:41 -0500

    Food API

--- otherScript.js
+++ otherScript.js
@@ -1,7 +1,7 @@
 var request = new XMLHttpRequest();
 
 function fireAPI() {
-    request.open('GET', 'https://dog.ceo/dog-api/documentation/random')
+    request.open('GET', 'https://static.openfoodfacts.org/data/openfoodfacts-mongodbdump.tar.gz')
     request.setRequestHeader('Accept', 'application/json'); // setting format response as json
     request.onload = function () {
         // Begin accessing JSON data here
